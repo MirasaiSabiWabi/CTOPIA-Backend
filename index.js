51e84d0d0c1f9bf445d94f9be3a3c16b371efe3d
MirasaiSabiWabi <inddevcommunity@gmail.com>
2024-07-13 21:47:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path = require('path'); // Importing path module to resolve file paths
 const app = express();
+const path = require('path'); // Added for path resolution
 const bodyParser = require('body-parser');
 
+// Middleware for handling CORS and headers
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
@@ -11,24 +12,27 @@ app.use(function (req, res, next) {
     );
     next();
 });
+
+// Middleware for parsing URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware for logging requests
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
+// Middleware for parsing JSON bodies
 app.use(express.json());
 
+// Route for serving dashboard.html on POST to /player/login/dashboard
 app.post('/player/login/dashboard', (req, res) => {
-    res.sendFile(__dirname + '/public/html/dashboard.html');
+    res.sendFile(path.join(__dirname, 'public', 'html', 'dashboard.html'));
 });
 
+// Route for validating login and responding with a token
 app.post('/player/growid/login/validate', (req, res) => {
-    // Extracting data from the request body
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
+    const { _token, growId, password } = req.body;
 
     const token = Buffer.from(
         `_token=${_token}&growId=${growId}&password=${password}`,
@@ -39,15 +43,17 @@ app.post('/player/growid/login/validate', (req, res) => {
     );
 });
 
+// Route for closing the window
 app.post('/player/validate/close', function (req, res) {
     res.send('<script>window.close();</script>');
 });
 
+// Route for serving main.html from the "web" folder on GET to /
 app.get('/', function (req, res) {
-       res.send('Hello World!');
-   // res.sendFile(path.join(__dirname, 'web', 'main.html'));
+    res.sendFile(path.join(__dirname, 'web', 'main.html'));
 });
 
+// Start the server and listen on port 5000
 app.listen(5000, function () {
     console.log('Listening on port 5000');
 });
