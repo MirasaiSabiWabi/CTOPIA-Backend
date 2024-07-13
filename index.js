98efc37eb31d57e1d3a336fa5519eed0eee2e778
MirasaiSabiWabi <inddevcommunity@gmail.com>
2024-07-13 21:59:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-// Middleware for handling CORS and headers
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
@@ -13,22 +13,22 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Serving static files from the "public" directory
+
 app.use(express.static(__dirname + '/public'));
 
-// Middleware for parsing URL-encoded bodies
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Middleware for logging requests
+
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
-// Middleware for parsing JSON bodies
+
 app.use(express.json());
 
-// Route for serving dashboard.html on GET and POST to /player/login/dashboard
+
 app.route('/player/login/dashboard')
     .get((req, res) => {
         res.sendFile(__dirname + '/public/html/dashboard.html');
@@ -37,7 +37,7 @@ app.route('/player/login/dashboard')
         res.sendFile(__dirname + '/public/html/dashboard.html');
     });
 
-// Route for validating login and responding with a token on GET and POST to /player/growid/login/validate
+
 app.route('/player/growid/login/validate')
     .get((req, res) => {
         const _token = req.query._token;
@@ -68,7 +68,7 @@ app.route('/player/growid/login/validate')
         );
     });
 
-// Route for closing the window on GET and POST to /player/validate/close
+
 app.route('/player/validate/close')
     .get((req, res) => {
         res.send('<script>window.close();</script>');
@@ -77,12 +77,12 @@ app.route('/player/validate/close')
         res.send('<script>window.close();</script>');
     });
 
-// Route for serving the main page on GET to /
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-// Start the server and listen on port 5000
+
 app.listen(5000, function () {
     console.log('Listening on port 5000');
 });
